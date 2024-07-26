ca44d79f3288da0044649f437ae5ba97b7c7645b
Nguyễn Thành Dâng <160807759+thanhdn123@users.noreply.github.com>
2024-07-26 11:14:37 +0700

    Add files via upload

--- Client.py
+++ Client.py
@@ -1,101 +1,310 @@
-import json
-import os
 import socket
-import time
 import signal
 import sys
+import os
+import time
+import threading
 
-HOST = '127.0.0.1'
-PORT = 12346
-OUTPUT_DIRECTORY = 'output'
-FORMAT = "utf-8"
+timeToScan = [0,0]
+check_ctrl_C= 1 
 
-def read_file_list_from_json(file_name):
-    if os.path.exists(file_name):
-        with open(file_name, 'r') as f:
-            try:
-                file_data = json.load(f)
-                return file_data
-            except json.JSONDecodeError: 
-                return []
-    return []
+def timer(list, check): 
+    list[1], list[0] = 0, 0
+    while True: 
+        if check == 0 : break 
+        time.sleep(1) 
+        list[1] += 1
 
-def file_exists(file_path):
-    return os.path.isfile(file_path)
+def printPercentage(lengthOfEachFile, total_lengOfEachFile, file_pointer):
+    for items in file_pointer:
+        print(f"Downloaded {items.name}: {round((1 - lengthOfEachFile[file_pointer.index(items)] / total_lengOfEachFile[file_pointer.index(items)]) * 100, 2)}%")
 
-def convert_size_to_bytes(size_str):
-    size_str = size_str.upper()
-    if size_str.endswith('MB'):
-        return int(size_str[:-2]) * 1024 * 1024
-    elif size_str.endswith('GB'):
-        return int(size_str[:-2]) * 1024 * 1024 * 1024
-    else:
-        raise ValueError("Invalid size format")
+def clearLinePercentage(file_pointer):
+    for _ in file_pointer:
+        print("\033[F\033[K", end='')
 
-def signal_func(sig, frame):
-    print("\nClient closing")
-    sys.exit(0)
-
-def main():
-    if not os.path.exists(OUTPUT_DIRECTORY):
-        os.makedirs(OUTPUT_DIRECTORY)
-
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST, PORT))
-        FILE_LIST_FROM_SERVER = []
-
-        while True:
-            name = s.recv(700).decode(FORMAT)
-            if name == 'END_LIST':
+def fillterDataNotFoundAndPrint(list404, list_file_not_found):
+    for items in list404:
+        check = False
+        for key in list_file_not_found:
+            if items == key:
+                check = True
                 break
-            s.sendall('ACK'.encode(FORMAT)) 
-            size = s.recv(1024).decode(FORMAT)
-            s.sendall('ACK'.encode(FORMAT))
-            FILE_LIST_FROM_SERVER.append({'file_name': name, 'size': size})
+        if check == False:
+            list_file_not_found.append(items)
+            print(f"File {items['filename']} not found in the list")
 
+def fillterData(data, file_list, list404, list_download):
+    for items in data:
+        check = False
+        for i in range(len(file_list)):
+            if items["filename"].strip() == file_list[i]["filename"]:
+                list_download.append(items)
+                check = True
+                break
+        if check == False:
+            list404.append(items)
+
+def readRequiredData(file_path):
+    data = []
+    with open(file_path, "r") as file:
+        while True:
+            line = file.readline()
+            if not line:
+                break
+            idxSpace = line.find(" ")
+            if idxSpace != -1:
+                temp = {}
+                temp["filename"] = line[:idxSpace]
+                temp["mode"] = removeEndLine(line[idxSpace + 1:])
+                data.append(temp)
+    return data
+
+def setLengthOfEachFile(data_require, file_list, lengthOfEachFile, total_lengOfEachFile):
+    for items in data_require:
+        size = 0
+        for i in range(len(file_list)):
+            if items["filename"].strip() == file_list[i]["filename"]:
+                size = file_list[i]["size"]
+                break
+        lengthOfEachFile.append(size)
+        total_lengOfEachFile.append(size)
+
+def noficationFinishDownload():
+    print("\nDownload completed!")
+    # print("Please update the input.txt file and Ctrl+S to continue downloading!")
+    # print("Or press Ctrl+C to exit!")
+
+def removeEndLine(str):
+	if str[len(str) - 1] == "\n":
+		return str[:len(str) - 1]
+	return str
+
+def UpLoadProcess(lengthOfEachFile):
+    for x in lengthOfEachFile:
+        if x != 0:
+            return False
+    return True
+
+def signal_handler(sig, frame, check):
+    print('You pressed Ctrl+C! Program will exit!')
+    check = 0 
+    time.sleep(1)
+    sys.exit(0)
+    
+    
+def main():
+
+    HOST = "127.0.0.1" # The server's hostname or IP address
+    PORT = 65432 # The port used by the server
+    FORMAT = "utf-8"
+    nameTypeOfData = ["B","KB","MB","GB","TB"]
+    BUFSIZE = 1024
+    ACK = ""
+    modeName = ["NORMAL", "HIGH", "CRITICAL"]
+    modeDownLoad = [1, 4, 10]
+    output_folder = 'output'
+    scanTime = 10
+
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    print("CLIENT SIDE")
+    client.connect((HOST, PORT))
+    print("client address:", client.getsockname())
+
+    # Receive the content of the JSON file from the server
+    file_list = []
+    sizeOfFileList = int(client.recv(BUFSIZE).decode(FORMAT))
+    print("\nNumber of file in list: ", sizeOfFileList)
+    
+    while sizeOfFileList > 0:
+        temp = {
+            "filename": "file1.txt",
+            "size": 1024,
+            "typeData": "KB"
+        }
+        temp["filename"] = client.recv(BUFSIZE).decode(FORMAT)
+        client.send("ACK".encode(FORMAT))
+        temp["size"] = float(client.recv(BUFSIZE).decode(FORMAT))
+        client.send("ACK".encode(FORMAT))
+        temp["typeData"] = client.recv(BUFSIZE).decode(FORMAT)
+        client.send("ACK".encode(FORMAT))
+        file_list.append(temp)
+        sizeOfFileList -= 1
         
-        print(f"List of files from server: ")
-        for file in FILE_LIST_FROM_SERVER:
-            print(f"File name: {file['file_name']} | Size: {file['size']}")
+    # Print the content of the JSON file
+    for items in file_list:
+        print(f"File name: {items['filename']}, File size: {items['size']} {items['typeData']}")
 
-        file_name = 'input.json'
+
+    # Convert the size of each file to bytes
+    for i in range(len(file_list)):
+        for j in range(len(nameTypeOfData)):
+            if file_list[i]["typeData"] == nameTypeOfData[j]:
+                file_list[i]["size"] = file_list[i]["size"] * (1024 ** j)
+                break
+    
+    data_require_before = []
+    list_file_not_found = []
+    total_file = 0
+    
+    try:
+        # set the time to read file 
+        thread = threading.Thread(target = timer , args = (timeToScan, check_ctrl_C)) 
+        thread.daemon = True 
+        thread.start()
 
         while True:
-            FILE_INPUT = read_file_list_from_json(file_name)
-            for file in FILE_INPUT:
-                FILE_NAME = file['filename']
-                SIZE = file['size']
-                FILE_PATH = os.path.join(OUTPUT_DIRECTORY, FILE_NAME)
+            # Load required data from the input.txt file 
+            #Remove end line of each line 
+            if(timeToScan[1] - timeToScan[0] < scanTime):
+                    continue
+            else : timeToScan[0] = timeToScan[1] 
 
-                if not file_exists(FILE_PATH):
-                    s.sendall(FILE_NAME.encode(FORMAT))
-                    response = s.recv(1024).decode(FORMAT)
+            data_require_after = readRequiredData("input.txt")
+            
+            total_file = len(data_require_after)
+            
+            #Remove file not in the list
+            list404 = []
+            list_download = []
+            
+            #Fillter data
+            fillterData(data_require_after, file_list, list404, list_download)
                     
-                    if response == 'START':
-                        s.sendall('ACK'.encode(FORMAT))
-                        with open(FILE_PATH, 'wb') as f:
-                            TOTAL_SIZE = convert_size_to_bytes(SIZE)
-                            RECEIVED_SIZE = 0
+            data_require_after = list_download
+            
+            fillterDataNotFoundAndPrint(list404, list_file_not_found)
+            
+            sizeBefore = len(data_require_before)
+            sizeAfter = len(data_require_after)
+            
+            if sizeBefore == sizeAfter:
+                continue
 
-                            while True:
-                                data = s.recv(1024)
-                                if data.endswith(b'END'):
-                                    f.write(data[:-3])
-                                    break
-                                f.write(data)
-                                RECEIVED_SIZE += len(data)
-                                NUMBER = round((RECEIVED_SIZE / TOTAL_SIZE) * 100 ) 
-                                print(f"Downloading {FILE_NAME}: {NUMBER:.2f}% complete", end='\r')
-                        print(f"\nDownload of {FILE_NAME} complete")
-                    else:
-                        print(response)
-            time.sleep(2)
+            # Send the required data to the server
+            data_require_after = data_require_after[sizeBefore:]
+            client.send(str(len(data_require_after)).encode(FORMAT))
+            ACK = client.recv(BUFSIZE).decode(FORMAT)
 
-if __name__ == '__main__':
-    signal.signal(signal.SIGINT, signal_func)
-    try:
-        main()
-    except KeyboardInterrupt:
-        print("\nClient terminated.")
+            for i in range(len(data_require_after)):
+                client.send(data_require_after[i]["filename"].encode(FORMAT))
+                ACK = client.recv(BUFSIZE).decode(FORMAT)
+                client.send(data_require_after[i]["mode"].encode(FORMAT))
+                ACK = client.recv(BUFSIZE).decode(FORMAT)
 
+            # Receive the required data from the server
+            file_pointer = []
+            for items in data_require_after:
+                file_pointer.append(open("output/"+items["filename"], "wb"))
+                
+                
+            lengthOfEachFile = []
+            total_lengOfEachFile = []
+            setLengthOfEachFile(data_require_after, file_list, lengthOfEachFile, total_lengOfEachFile)
+                
+            # #print percentage of each file download    
+            for items in data_require_after:
+                print(f"Downloading {items['filename']} with mode {items['mode']}")
+          
+            
+            while UpLoadProcess(lengthOfEachFile) == False:
+                for items in file_pointer:
+                    if lengthOfEachFile[file_pointer.index(items)] <= 0:
+                        continue
+                    modeNumer = 1
+                    for i in range(len(modeName)):
+                        if data_require_after[file_pointer.index(items)]["mode"] == modeName[i]:
+                            modeNumer = modeDownLoad[i]
+                            break
+                    for i in range(modeNumer):
+                        if lengthOfEachFile[file_pointer.index(items)] <= 0:
+                            continue
+                        client.send("ACK".encode(FORMAT))
+                        data = client.recv(BUFSIZE)
+                        items.write(data)
+                        lengthOfEachFile[file_pointer.index(items)] -= len(data)
+                
+                printPercentage(lengthOfEachFile, total_lengOfEachFile, file_pointer)
+                clearLinePercentage(file_pointer)
+                
+
+                if(timeToScan[1] - timeToScan[0] < scanTime):
+                    client.send("0".encode(FORMAT))
+                    client.recv(BUFSIZE).decode(FORMAT)
+                    continue
+                else : timeToScan[0] = timeToScan[1]
+
+                
+                update_data_require_after = readRequiredData("input.txt")
+                
+                total_new_file = len(update_data_require_after) - total_file
+                
+                if total_new_file <= 0:
+                    client.send("0".encode(FORMAT))
+                    ACK = client.recv(BUFSIZE).decode(FORMAT)
+                    continue
+                    
+                update_data_require_after = update_data_require_after[total_file:]
+                
+                #Remove file not in the list
+                new_list404 = []
+                new_list_download = []
+                fillterData(update_data_require_after, file_list, new_list404, new_list_download)
+                
+                update_data_require_after = new_list_download
+                
+                #print file not in the list
+                fillterDataNotFoundAndPrint(new_list404, list_file_not_found)
+                
+                #check need to download new file or not
+                if len(update_data_require_after) == 0:
+                    total_file += total_new_file
+                    client.send("0".encode(FORMAT))
+                    ACK = client.recv(BUFSIZE).decode(FORMAT)
+                    continue
+                
+                #Send new required file to the server
+                client.send(str(len(update_data_require_after)).encode(FORMAT))
+                ACK = client.recv(BUFSIZE).decode(FORMAT)
+                
+                #Print notification for new required file
+                for items in update_data_require_after:
+                    print(f"Downloading {items['filename']} with mode {items['mode']}")
+                    data_require_after.append(items)  
+                
+                for i in range(len(update_data_require_after)):
+                    client.send(update_data_require_after[i]["filename"].encode(FORMAT))
+                    ACK = client.recv(BUFSIZE).decode(FORMAT)
+                    client.send(update_data_require_after[i]["mode"].encode(FORMAT))
+                    ACK = client.recv(BUFSIZE).decode(FORMAT)
+
+                # Make the list file pointer
+                for items in update_data_require_after:
+                    file_pointer.append(open("output/"+items["filename"], "wb"))
+                    
+                #Set the length of each new file    
+                setLengthOfEachFile(update_data_require_after, file_list, lengthOfEachFile, total_lengOfEachFile)
+                    
+                total_file += total_new_file
+#----------------------------------------------------------------------------------------------
+            
+            for items in file_pointer:
+                items.close()
+                
+            for items in data_require_after:
+                data_require_before.append(items)
+            
+            noficationFinishDownload()
+            
+    except Exception as e:
+        client.close()
+        
+        
+if __name__ == "__main__":
+    signal.signal(signal.SIGINT, signal_handler)
+    main()
 
--- Server.py
+++ Server.py
@@ -1,63 +1,200 @@
-import os
 import socket
+import threading
 import json
+import os
 
-HOST = '127.0.0.1'
-PORT = 12346
-FILE_DIRECTORY = './files'
-FILE_LIST_FILENAME = 'list.json'
-FORMAT = "utf-8"
+def convert_bytes(file_size):
+	nameOfTypeData = ["B", "KB", "MB", "GB", "TB"]
+	sizeOfTypeData = len(nameOfTypeData)
+	for i in range(sizeOfTypeData):
+		if file_size < 1024 or file_size%1024 != 0:
+			return file_size, nameOfTypeData[i]
+		file_size = file_size / 1024
 
-def load_file_list():
-    with open(FILE_LIST_FILENAME, 'r') as f:
-        return json.load(f)
+def write_file_list_to_json(folder_path, output_file='file_list.json'):
+    # List to hold file information
+    file_info_list = []
 
-FILE_LIST = load_file_list()
+    # Walk through the directory
+    for root, dirs, files in os.walk(folder_path):
+        for file in files:
+            # Get the full file path
+            file_path = os.path.join(root, file)
+            # Get the file size in MB
+            file_size = os.path.getsize(file_path)
+            size, typeData = convert_bytes(file_size)
+            # Append file information to the list
+            file_info_list.append({
+                "filename": file,
+                "size": size, 
+                "typeData": typeData
+            })
 
-def file_exists(file_path):
-    return os.path.isfile(file_path)
+    # Write the list to a JSON file
+    with open(output_file, 'w') as json_file:
+        json.dump(file_info_list, json_file, indent=4)
 
-def main():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, PORT))
-        s.listen(5)
-        print(f"Server listening on {HOST}:{PORT}")
-        while True:
-            client_socket, addr = s.accept()
-            print(f"Connection from {addr}")
-            try:
-                for file in FILE_LIST:
-                    client_socket.sendall(file['filename'].encode(FORMAT))
-                    client_socket.recv(1024).decode(FORMAT)
-                    client_socket.sendall(str(file['size']).encode(FORMAT))
-                    client_socket.recv(1024).decode(FORMAT) 
-                client_socket.sendall('END_LIST'.encode(FORMAT))
-
-                
-                while True:
-                    FILE_NAME = client_socket.recv(1024).decode(FORMAT)
-                    if FILE_NAME == "END":
+def setLengthOfEachFile(data_require, file_list, lengthOfEachFile, nameTypeOfData):
+    for items in data_require:
+        for i in range(len(file_list)):
+            if file_list[i]['filename'] == items["filename"]:
+                for j in range(len(nameTypeOfData)):
+                    if file_list[i]['typeData'] == nameTypeOfData[j]:
+                        lengthOfEachFile.append(int(file_list[i]['size'] * (1024 ** j)))
                         break
+                break
 
-                    FILE_PATH = os.path.join(FILE_DIRECTORY, FILE_NAME)
+def UpLoadProcess(lengthOfEachFile):
+    for x in lengthOfEachFile:
+        if x != 0:
+            return False
+    return True
 
-                    if file_exists(FILE_PATH):
-                        client_socket.sendall('START'.encode(FORMAT))
-                        client_socket.recv(1024).decode(FORMAT)
-                        with open(FILE_PATH, 'rb') as file:
-                            while True:
-                                data = file.read(1024) 
-                                if not data : break 
-                                client_socket.sendall(data) 
-                            
-                        client_socket.sendall('END'.encode(FORMAT))
-                    else:
-                        client_socket.sendall('ERROR: File not found'.encode(FORMAT))
-            except Exception as e:
-                print(f"Closing connection with {addr}" )
-            finally:
-                client_socket.close()
+data_path = "C:\\Users\\dn156\\source\\Networking\\Socket\\Part_2\\files"
+file_path = "file_list.json"
+HOST = "127.0.0.1" # The server's hostname or IP address
+PORT = 65432 # The port used by the server
+FORMAT = "utf-8"
+BUFSIZE = 1024
+nameTypeOfData = ["B","KB","MB","GB","TB"]
+sizeTypeOfData = len(nameTypeOfData)
+modeName = ["NORMAL", "HIGH", "CRITICAL"]
+modeDownLoad = [1, 4, 10]
 
-if __name__ == '__main__':
-    main() 
+def handle_client(connection, address, file_list):
+	while True:
+		try:
+      
+			print("client address: ", address)
+			print("connection: ", connection.getsockname(), end="\n\n")
+			print("Server is connected to the client")
+			
+			connection.send(str(len(file_list)).encode(FORMAT))
+			for items in file_list:
+				connection.send(items['filename'].encode(FORMAT))
+				ACK = connection.recv(BUFSIZE).decode(FORMAT)
+				connection.send(str(items['size']).encode(FORMAT))
+				ACK = connection.recv(BUFSIZE).decode(FORMAT)
+				connection.send(items['typeData'].encode(FORMAT))
+				ACK = connection.recv(BUFSIZE).decode(FORMAT)
+			
+			try:
+				while True:
+					data_require = []
+					# Receive the required data from the client
+					sizeOfDataRequire = int(connection.recv(BUFSIZE).decode(FORMAT))
+					connection.send("ACK".encode(FORMAT))
+
+					while sizeOfDataRequire > 0:
+						temp = {}
+						temp["filename"] = connection.recv(BUFSIZE).decode(FORMAT)
+						connection.send("ACK".encode(FORMAT))
+						temp["mode"] = connection.recv(BUFSIZE).decode(FORMAT)
+						connection.send("ACK".encode(FORMAT))
+						data_require.append(temp)
+						sizeOfDataRequire -= 1
+
+					# Print the required data
+					print("Data required from the client with address: ", address)
+					for items in data_require:
+						print(f"File name: {items['filename']}, Mode: {items['mode']}")
+
+					# Set the length of each file
+					lengthOfEachFile = []
+					setLengthOfEachFile(data_require, file_list, lengthOfEachFile, nameTypeOfData)
+
+					# Make the list file pointer
+					file_pointer = []
+					for items in data_require:
+						path = data_path + "\\" + items["filename"]
+						file_pointer.append(open(path, "rb"))
+					
+					# Send the required data to the client
+					while UpLoadProcess(lengthOfEachFile) == False:
+						for items in file_pointer:
+							if lengthOfEachFile[file_pointer.index(items)] <= 0:
+								continue
+							modeNumer = 1
+							for i in range(len(modeName)):
+								if data_require[file_pointer.index(items)]["mode"] == modeName[i]:
+									modeNumer = modeDownLoad[i]
+									break
+							for i in range(modeNumer):
+								if lengthOfEachFile[file_pointer.index(items)] <= 0:
+									continue
+								buffer = items.read(BUFSIZE)
+								ACK = connection.recv(BUFSIZE).decode(FORMAT)
+								connection.send(buffer)
+								lengthOfEachFile[file_pointer.index(items)] -= len(buffer)
+						
+						total_new_items = int(connection.recv(BUFSIZE).decode(FORMAT))
+						connection.send("ACK".encode(FORMAT)) 
+						
+						if total_new_items == 0:
+							continue
+						
+						new_require_data = []
+						
+						while total_new_items > 0:
+							temp = {}
+							temp["filename"] = connection.recv(BUFSIZE).decode(FORMAT)
+							connection.send("ACK".encode(FORMAT))
+							temp["mode"] = connection.recv(BUFSIZE).decode(FORMAT)
+							connection.send("ACK".encode(FORMAT))
+							new_require_data.append(temp)
+							total_new_items -= 1
+							
+						print("Data required from the client with address: ", address)
+						for items in new_require_data:
+							print(f"File name: {items['filename']}, Mode: {items['mode']}")
+							data_require.append(items)
+						
+						# Set the length of each file
+						setLengthOfEachFile(new_require_data, file_list, lengthOfEachFile, nameTypeOfData)
+
+						# Make the list file pointer
+						for items in new_require_data:
+							path = data_path + "\\" + items["filename"]
+							file_pointer.append(open(path, "rb"))
+							
+					for item in file_pointer:
+						item.close()
+						
+					print(f"\nAll files are sent to the client has address {address}!\n")
+
+			except Exception as e:
+				print(f"Error: {e}")
+				break
+
+		except Exception as e:
+			print(f"Connection with the client has address {address} is closed!")
+			break
+  
+def main():
+	write_file_list_to_json(data_path, file_path)
+	# Open the JSON file and load its content
+	with open(file_path, "r") as file:
+		data = json.load(file)
+
+	#Create a server socket
+	server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	server.bind((HOST, PORT))
+	server.listen()
+
+	print("SERVER SIDE")
+	print(f"Server is listening on {HOST}:{PORT}")
+
+	print("Sever is waiting for client request...\n")
+
+	while True:
+		connection, address = server.accept()
+		thread = threading.Thread(target=handle_client, args=(connection, address, data))
+		thread.daemon = False
+		thread.start()
+ 
+if __name__ == "__main__":
+	print(1)
+	main()
+	input("Press Enter to continue...")
+
 
