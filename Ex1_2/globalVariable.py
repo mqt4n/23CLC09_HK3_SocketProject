c6ae7af2b8d7d94e376e2a3eb328a9292f8ba8f3
GinKz <146076090+Meowmeok@users.noreply.github.com>
2024-08-07 11:25:36 +0700

    Add files via upload

---
+++ Ex1_2/globalVariable.py
@@ -0,0 +1,10 @@
+# global variable in both file
+def init():
+	global announFirst
+	global announSecond
+	global downloading
+	global endProgram
+	announFirst = ''
+	announSecond = ''
+	downloading = False
+	endProgram = False
