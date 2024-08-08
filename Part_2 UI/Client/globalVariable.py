ce531d2d57993268a37b07a57bdba8fe5f72e029
GinKz <146076090+Meowmeok@users.noreply.github.com>
2024-08-08 16:06:58 +0700

    Add files via upload

---
+++ Part_2 UI/Client/globalVariable.py
@@ -0,0 +1,11 @@
+# global variable in both file
+def init():
+	global announFirst
+	global announSecond
+	global downloading
+	global endProgram
+
+	announFirst = ''
+	announSecond = ''
+	downloading = False
+	endProgram = False
