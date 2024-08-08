c4ce7f40c8299ad85d76470dea8a5cb1228d71ae
Nguyễn Thành Dâng <160807759+thanhdn123@users.noreply.github.com>
2024-08-08 14:29:09 +0700

    Update Client.py

--- Part_1/Client.py
+++ Part_1/Client.py
@@ -53,79 +53,82 @@ def main():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
         FILE_LIST_FROM_SERVER = []
+        try:
+            while True:
+                name = s.recv(1024).decode(FORMAT)
+                if name == 'END_LIST':
+                    break
+                s.sendall('ACK'.encode(FORMAT)) 
+                size = s.recv(1024).decode(FORMAT)
+                s.sendall('ACK'.encode(FORMAT))
+                type = s.recv(1024).decode(FORMAT) 
+                s.sendall('ACK'.encode(FORMAT)) 
+                FILE_LIST_FROM_SERVER.append({'file_name': name, 'size': size, 'typeData': type}) 
 
-        while True:
-            name = s.recv(1024).decode(FORMAT)
-            if name == 'END_LIST':
-                break
-            s.sendall('ACK'.encode(FORMAT)) 
-            size = s.recv(1024).decode(FORMAT)
-            s.sendall('ACK'.encode(FORMAT))
-            type = s.recv(1024).decode(FORMAT) 
-            s.sendall('ACK'.encode(FORMAT)) 
-            FILE_LIST_FROM_SERVER.append({'file_name': name, 'size': size, 'typeData': type}) 
+            
+            print(f"List of files from server: ")
+            for file in FILE_LIST_FROM_SERVER:
+                if file['typeData']=='B':
+                    size = int(file['size'])
+                    index = 0
+                    while size>=1024:
+                        size/=1024
+                        index+=1
+                    print(f"File name: {file['file_name']} | Size: {round(size,2)} {nameOfTypeData[index]}")
+                else:
+                    print(f"File name: {file['file_name']} | Size: {file['size']} {file['typeData']}")
 
-        
-        print(f"List of files from server: ")
-        for file in FILE_LIST_FROM_SERVER:
-            if file['typeData']=='B':
-                size = int(file['size'])
-                index = 0
-                while size>=1024:
-                      size/=1024
-                      index+=1
-                print(f"File name: {file['file_name']} | Size: {round(size,2)} {nameOfTypeData[index]}")
-            else:
-                print(f"File name: {file['file_name']} | Size: {file['size']} {file['typeData']}")
+            NOT_FOUND = []
+            CHECK = []
+            while True: 
+                file_name = 'input.txt' 
+                FILE_INPUT = readFile(file_name) 
+                if FILE_INPUT == CHECK : break 
+                else: CHECK = FILE_INPUT 
+                for file in FILE_INPUT:
+                    
+                    FILE_NAME = file
+                    SIZE = 0 
+                    TYPE = ''
+                    for f in FILE_LIST_FROM_SERVER: 
+                        if f['file_name'] == FILE_NAME : 
+                            SIZE = f['size']  
+                            TYPE = f['typeData'] 
+                            
+                    FILE_PATH = os.path.join(OUTPUT_DIRECTORY, FILE_NAME)
 
-        NOT_FOUND = []
-        CHECK = []
-        while True: 
-            file_name = 'input.txt' 
-            FILE_INPUT = readFile(file_name) 
-            if FILE_INPUT == CHECK : break 
-            else: CHECK = FILE_INPUT 
-            for file in FILE_INPUT:
+                    check = 0 
+                    for i in NOT_FOUND: 
+                        if i == FILE_NAME: 
+                            check = 1 
+                        
+                    if not file_exists(FILE_PATH) and check == 0 : 
+                        s.sendall(FILE_NAME.encode(FORMAT))
+                        response = s.recv(1024).decode(FORMAT)
+                        if response == 'START' :
+                            s.sendall('ACK'.encode(FORMAT))
+                            with open(FILE_PATH, 'wb') as f:
+                                TOTAL_SIZE = convert_size_to_bytes(SIZE, TYPE )
+                                RECEIVED_SIZE = 0
+                                while True :
+                                    data = s.recv(1024)
+                                    RECEIVED_SIZE += len(data)
+                                    if TOTAL_SIZE != 0 : 
+                                        NUMBER = round((RECEIVED_SIZE / TOTAL_SIZE) * 100 ) 
+                                    else : NUMBER = 100 
+                                    if NUMBER >= 100 : NUMBER = 100  
+                                    print(f"Downloading {FILE_NAME}: {NUMBER:.2f}% complete", end='\r')
+                                    if data.endswith(b'END'):
+                                        f.write(data[:-3])
+                                        break
+                                    f.write(data)
+                            print(f"\nDownload of {FILE_NAME} complete")
+                        else : 
+                            print(str(FILE_NAME) + " " + response ) 
+                            NOT_FOUND.append(FILE_NAME)    
+        except Exception as e : 
+             print("")
                 
-                FILE_NAME = file
-                SIZE = 0 
-                TYPE = ''
-                for f in FILE_LIST_FROM_SERVER: 
-                     if f['file_name'] == FILE_NAME : 
-                          SIZE = f['size']  
-                          TYPE = f['typeData'] 
-                          
-                FILE_PATH = os.path.join(OUTPUT_DIRECTORY, FILE_NAME)
-
-                check = 0 
-                for i in NOT_FOUND: 
-                     if i == FILE_NAME: 
-                          check = 1 
-                     
-                if not file_exists(FILE_PATH) and check == 0 : 
-                    s.sendall(FILE_NAME.encode(FORMAT))
-                    response = s.recv(1024).decode(FORMAT)
-                    if response == 'START' :
-                        s.sendall('ACK'.encode(FORMAT))
-                        with open(FILE_PATH, 'wb') as f:
-                            TOTAL_SIZE = convert_size_to_bytes(SIZE, TYPE )
-                            RECEIVED_SIZE = 0
-                            while True :
-                                data = s.recv(1024)
-                                RECEIVED_SIZE += len(data)
-                                if TOTAL_SIZE != 0 : 
-                                    NUMBER = round((RECEIVED_SIZE / TOTAL_SIZE) * 100 ) 
-                                else : NUMBER = 100 
-                                if NUMBER >= 100 : NUMBER = 100  
-                                print(f"Downloading {FILE_NAME}: {NUMBER:.2f}% complete", end='\r')
-                                if data.endswith(b'END'):
-                                    f.write(data[:-3])
-                                    break
-                                f.write(data)
-                        print(f"\nDownload of {FILE_NAME} complete")
-                    else : 
-                        print(str(FILE_NAME) + " " + response ) 
-                        NOT_FOUND.append(FILE_NAME)    
 
 if __name__ == '__main__':
     signal.signal(signal.SIGINT, signal_func)
